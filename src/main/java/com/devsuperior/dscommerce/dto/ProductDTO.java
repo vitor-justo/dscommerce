a68069ba24b69a4eec00e735b5735017cef68d90
vitor-justo <24323995+vitor-justo@users.noreply.github.com>
2025-03-11 12:48:26 -0300

    Validation handling

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -2,13 +2,25 @@ package com.devsuperior.dscommerce.dto;
 
 
 import com.devsuperior.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
     private Long id;
+
+    @Size(min = 3, max = 80, message = "Nome precisa ter entre 3 e 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+
+    @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+
+    @Positive(message = "Preço deve ser positivo")
     private Double price;
+
     private String imgUrl;
 
     public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
